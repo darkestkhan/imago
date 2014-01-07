b6a66822c3c912c07082909c88fe57f734dfc3eb
darkestkhan <darkestkhan@gmail.com>
2014-01-07 18:18:34 +0000

    Finish binding to #defines.
    Signed-off-by: darkestkhan <darkestkhan@gmail.com>

--- src/imago-il.ads
+++ src/imago-il.ads
@@ -261,6 +261,120 @@ package Imago.IL is
   IL_NO_COMPRESSION                       : constant Enum := 16#0667#;
   IL_COMPRESSION_HINT                     : constant Enum := 16#0668#;
 
+  -- Compression.
+  IL_NVIDIA_COMPRESS                      : constant Enum := 16#0670#;
+  IL_SQUISH_COMPRESS                      : constant Enum := 16#0671#;
+
+  -- Subimage types.
+  IL_SUB_NEXT                             : constant Enum := 16#0680#;
+  IL_SUB_MIPMAP                           : constant Enum := 16#0681#;
+  IL_SUB_LAYER                            : constant Enum := 16#0682#;
+
+  -- Compression definitions.
+  IL_COMPRESS_MODE                        : constant Enum := 16#0700#;
+  IL_COMPRESS_NONE                        : constant Enum := 16#0701#;
+  IL_COMPRESS_RLE                         : constant Enum := 16#0702#;
+  IL_COMPRESS_LZO                         : constant Enum := 16#0703#;
+  IL_COMPRESS_ZLIB                        : constant Enum := 16#0704#;
+
+  -- File format specific values.
+  IL_TGA_CREATE_STAMP                     : constant Enum := 16#0710#;
+  IL_JPG_QUALITY                          : constant Enum := 16#0711#;
+  IL_PNG_INTERLACE                        : constant Enum := 16#0712#;
+  IL_TGA_RLE                              : constant Enum := 16#0713#;
+  IL_BMP_RLE                              : constant Enum := 16#0714#;
+  IL_SGI_RLE                              : constant Enum := 16#0715#;
+  IL_TGA_ID_STRING                        : constant Enum := 16#0717#;
+  IL_TGA_AUTHNAME_STRING                  : constant Enum := 16#0718#;
+  IL_TGA_AUTHCOMMENT_STRING               : constant Enum := 16#0719#;
+  IL_PNG_AUTHNAME_STRING                  : constant Enum := 16#071A#;
+  IL_PNG_TITLE_STRING                     : constant Enum := 16#071B#;
+  IL_PNG_DESCRIPTION_STRING               : constant Enum := 16#071C#;
+  IL_TIF_DESCRIPTION_STRING               : constant Enum := 16#071D#;
+  IL_TIF_HOSTCOMPUTER_STRING              : constant Enum := 16#071E#;
+  IL_TIF_DOCUMENTNAME_STRING              : constant Enum := 16#071F#;
+  IL_TIF_AUTHNAME_STRING                  : constant Enum := 16#0720#;
+  IL_JPG_SAVE_FORMAT                      : constant Enum := 16#0721#;
+  IL_CHEAD_HEADER_STRING                  : constant Enum := 16#0722#;
+  IL_PCD_PICNUM                           : constant Enum := 16#0723#;
+  IL_PNG_ALPHA_INDEX                      : constant Enum := 16#0724#;
+  IL_JPG_PROGRESSIVE                      : constant Enum := 16#0725#;
+  IL_VTF_COMP                             : constant Enum := 16#0726#;
+
+  -- DXTC definitions.
+  IL_DXTC_FORMAT                          : constant Enum := 16#0705#;
+  IL_DXT1                                 : constant Enum := 16#0706#;
+  IL_DXT2                                 : constant Enum := 16#0707#;
+  IL_DXT3                                 : constant Enum := 16#0708#;
+  IL_DXT4                                 : constant Enum := 16#0709#;
+  IL_DXT5                                 : constant Enum := 16#070A#;
+  IL_DXT_NO_COMP                          : constant Enum := 16#070B#;
+  IL_KEEP_DXTC_DATA                       : constant Enum := 16#070C#;
+  IL_DXTC_DATA_FORMAT                     : constant Enum := 16#070D#;
+  IL_3DC                                  : constant Enum := 16#070E#;
+  IL_RXGB                                 : constant Enum := 16#070F#;
+  IL_ATI1N                                : constant Enum := 16#0710#;
+  IL_DXT1A                                : constant Enum := 16#0711#;
+
+  -- Environment map definitions.
+  IL_CUBEMAP_POSITIVEX                    : constant Bitfield := 16#00000400#;
+  IL_CUBEMAP_NEGATIVEX                    : constant Bitfield := 16#00000800#;
+  IL_CUBEMAP_POSITIVEY                    : constant Bitfield := 16#00001000#;
+  IL_CUBEMAP_NEGATIVEY                    : constant Bitfield := 16#00002000#;
+  IL_CUBEMAP_POSITIVEZ                    : constant Bitfield := 16#00004000#;
+  IL_CUBEMAP_NEGATIVEZ                    : constant Bitfield := 16#00008000#;
+  IL_SPHEREMAP                            : constant Bitfield := 16#00010000#;
+
+  -- Values.
+  IL_VERSION_NUM                          : constant Enum := 16#0DE2#;
+  IL_IMAGE_WIDTH                          : constant Enum := 16#0DE4#;
+  IL_IMAGE_HEIGHT                         : constant Enum := 16#0DE5#;
+  IL_IMAGE_DEPTH                          : constant Enum := 16#0DE6#;
+  IL_IMAGE_SIZE_OF_DATA                   : constant Enum := 16#0DE7#;
+  IL_IMAGE_BPP                            : constant Enum := 16#0DE8#;
+  IL_IMAGE_BYTES_PER_PIXEL                : constant Enum := 16#0DE8#;
+  IL_IMAGE_BITS_PER_PIXEL                 : constant Enum := 16#0DE9#;
+  IL_IMAGE_FORMAT                         : constant Enum := 16#0DEA#;
+  IL_IMAGE_TYPE                           : constant Enum := 16#0DEB#;
+  IL_PALETTE_TYPE                         : constant Enum := 16#0DEC#;
+  IL_PALETTE_SIZE                         : constant Enum := 16#0DED#;
+  IL_PALETTE_BPP                          : constant Enum := 16#0DEE#;
+  IL_PALETTE_NUM_COLS                     : constant Enum := 16#0DEF#;
+  IL_PALETTE_BASE_TYPE                    : constant Enum := 16#0DF0#;
+  IL_NUM_FACES                            : constant Enum := 16#0DE1#;
+  IL_NUM_IMAGES                           : constant Enum := 16#0DF1#;
+  IL_NUM_MIPMAPS                          : constant Enum := 16#0DF2#;
+  IL_NUM_LAYERS                           : constant Enum := 16#0DF3#;
+  IL_ACTIVE_IMAGE                         : constant Enum := 16#0DF4#;
+  IL_ACTIVE_MIPMAP                        : constant Enum := 16#0DF5#;
+  IL_ACTIVE_LAYER                         : constant Enum := 16#0DF6#;
+  IL_ACTIVE_FACE                          : constant Enum := 16#0E00#;
+  IL_CUR_IMAGE                            : constant Enum := 16#0DF7#;
+  IL_IMAGE_DURATION                       : constant Enum := 16#0DF8#;
+  IL_IMAGE_PLANESIZE                      : constant Enum := 16#0DF9#;
+  IL_IMAGE_BPC                            : constant Enum := 16#0DFA#;
+  IL_IMAGE_OFFX                           : constant Enum := 16#0DFB#;
+  IL_IMAGE_OFFY                           : constant Enum := 16#0DFC#;
+  IL_IMAGE_CUBEFLAGS                      : constant Enum := 16#0DFD#;
+  IL_IMAGE_ORIGIN                         : constant Enum := 16#0DFE#;
+  IL_IMAGE_CHANNELS                       : constant Enum := 16#0DFF#;
+
+  IL_SEEK_SET	                            : constant Int  := 0;
+  IL_SEEK_CUR	                            : constant Int  := 1;
+  IL_SEEK_END	                            : constant Int  := 2;
+  IL_EOF		                              : constant Int  := -1;
+
+  --------------------------------------------------------------------------
+
+  -- NOTE/FIXME/TODO: typedefs of pointers to functions are not bound.
+  --                  Not sure if they are used in API or just internally.
+
+  --------------------------------------------------------------------------
+
+                        ---------------------------
+                        -- S U B P R O G R A M S --
+                        ---------------------------
+
   --------------------------------------------------------------------------
 
 end Imago.IL;
