4f020c1c11c6cfc5566011d53e0a85b3aa46b517
darkestkhan <darkestkhan@gmail.com>
2015-07-13 18:32:14 +0000

    Relicense to ISC license.
    Signed-off-by: darkestkhan <darkestkhan@gmail.com>

--- src/imago-ilut.ads
+++ src/imago-ilut.ads
@@ -1,30 +1,20 @@
-pragma License (Modified_GPL);
 ------------------------------------------------------------------------------
 -- EMAIL: <darkestkhan@gmail.com>                                           --
--- License: Modified GNU GPLv3 or any later as published by Free Software   --
---  Foundation (GMGPL, see COPYING file).                                   --
+-- License: ISC                                                             --
 --                                                                          --
 --                    Copyright © 2015 darkestkhan                          --
 ------------------------------------------------------------------------------
---  This Program is Free Software: You can redistribute it and/or modify    --
---  it under the terms of The GNU General Public License as published by    --
---    the Free Software Foundation: either version 3 of the license, or     --
---                 (at your option) any later version.                      --
+-- Permission to use, copy, modify, and/or distribute this software for any --
+-- purpose with or without fee is hereby granted, provided that the above   --
+-- copyright notice and this permission notice appear in all copies.        --
 --                                                                          --
---      This Program is distributed in the hope that it will be useful,     --
---      but WITHOUT ANY WARRANTY; without even the implied warranty of      --
---      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        --
---              GNU General Public License for more details.                --
---                                                                          --
---    You should have received a copy of the GNU General Public License     --
---   along with this program.  If not, see <http://www.gnu.org/licenses/>.  --
---                                                                          --
--- As a special exception,  if other files  instantiate  generics from this --
--- unit, or you link  this unit with other files  to produce an executable, --
--- this  unit  does not  by itself cause  the resulting  executable  to  be --
--- covered  by the  GNU  General  Public  License.  This exception does not --
--- however invalidate  any other reasons why  the executable file  might be --
--- covered by the  GNU Public License.                                      --
+-- The software is provided "as is" and the author disclaims all warranties --
+-- with regard to this software including all implied warranties of         --
+-- merchantability and fitness. In no event shall the author be liable for  --
+-- any special, direct, indirect, or consequential damages or any damages   --
+-- whatsoever resulting from loss of use, data or profits, whether in an    --
+-- action of contract, negligence or other tortious action, arising out of  --
+-- or in connection with the use or performance of this software.           --
 ------------------------------------------------------------------------------
 with Imago.IL;
 use Imago;
